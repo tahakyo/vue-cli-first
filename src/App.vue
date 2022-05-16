0d126d93b986662d92647119cfa0d523e8d035d9
tahakyo <ghazouani1988@email.com>
2022-05-16 17:56:26 +0200

    MyTitle created

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <MyTitle />
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -7,11 +8,12 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import MyTitle from "./components/MyTitle.vue";
 export default {
   name: "App",
   components: {
     HelloWorld,
+    MyTitle,
   },
 };
 </script>
